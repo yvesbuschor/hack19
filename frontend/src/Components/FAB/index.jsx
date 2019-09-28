2a0df861b7b8ae0e18ee991371bf4e63ae917035
Yves Buschor <yves.buschor@gmail.com>
2019-09-29 01:37:57 +0200

    set current location on fab click

--- frontend/src/Components/FAB/index.jsx
+++ frontend/src/Components/FAB/index.jsx
@@ -3,10 +3,9 @@ import { Link, withRouter } from 'react-router-dom';
 import routes from 'routes';
 import { makeStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
-import MapIcon from '@material-ui/icons/Map';
-import LocationSearchingIcon from '@material-ui/icons/LocationSearching';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 
+import { useStateValue } from 'state';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,6 +23,25 @@ const useStyles = makeStyles(theme => ({
 
 const FloatingActionButtons = ({ location }) => {
   const classes = useStyles();
+  const [, dispatch] = useStateValue();
+
+  const setGeolocation = (position) => {
+    dispatch({
+      type: 'create.dataUpdated',
+      data: {
+        lat: position.coords.latitude,
+        long: position.coords.longitude,
+      }
+    });
+  }
+
+  const getGeolocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(setGeolocation);
+    } else { 
+      alert("first select a point on the map");
+    }
+  }
 
   return (
     <div className={classes.root}>
@@ -33,6 +51,7 @@ const FloatingActionButtons = ({ location }) => {
         label="Add Idea"
         value={routes.ideaSubmit}
         className={classes.fab}
+        onClick={getGeolocation}
       ><AddCircleIcon /></Fab>
     </div>
   );
