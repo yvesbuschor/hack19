0c4a75dc5325945a3218cb079dc7b5d2390b82f3
Raphael Brunner <raphael@raphaelbrunner.ch>
2019-09-28 14:49:36 +0200

    adding some reducers

---
+++ frontend/src/reducer.js
@@ -0,0 +1,92 @@
+const reducer = ({ idea, location, comment }, action) => {
+  // middleware goes here, i.e calling analytics service, etc.
+  return {
+    idea: ideaReducer(idea, action),
+    location: locationReducer(location, action),
+    comment: commentReducer(comment, action)
+  };
+};
+
+  const ideaReducer = (state, action) => {
+    switch (action.type) {
+        case 'idea.changed':
+            return {
+            ...state,
+            data: action.ideas,
+            status: 'done'
+            };
+        
+        case 'idea.saved':
+            return {
+                ...state,
+                status: 'done',
+                data: action.idea
+            }
+
+        case 'idea.saving':
+        case 'idea.fetching':
+            return {
+                ...state,
+                status: 'pending'
+            };
+
+        case 'idea.failed':
+            return {
+                ...state,
+                status: 'fail'
+            }
+
+        default:
+            return state;
+    }
+  };
+  
+  const commentReducer = (state, action) => {
+      switch (action.type) {
+        case 'comment.save':
+            return {
+                ...state,
+                status: 'pending'
+            };
+        
+        case 'comment.saved':
+            return {
+                ...state,
+                status: 'done',
+                data: action.ideas
+            };
+
+        case 'comment.failed':
+            return {
+                ...state,
+                status: 'fail'
+            };
+        
+      }
+  };
+
+const locationReducer = (state, action) => {
+    switch (action.type) {
+        case 'location.fetching':
+            return {
+                ...state,
+                status: 'pending'
+            };
+    
+        case 'location.changed':
+            return {
+                ...state,
+                status: 'done',
+                data: action.locations
+            };
+        
+        case 'location.failed':
+            return {
+                ...state,
+                status: 'fail'
+            };
+        
+      default:
+        return state;
+    }
+  };
