d1e87f8ce28c800dd392652a5c8c013665f6d464
Yves Buschor <yves.buschor@gmail.com>
2019-09-28 14:58:48 +0200

    add bottom nav and theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import routes from 'routes';
+import theme from 'theme';
 import { StateProvider } from 'state';
 
-import LandingScene from './Scenes/Landing';
-import PlaygroundScene from './Scenes/Playground';
-import MapScene from './Scenes/Map';
-import IdeaDetailScene from './Scenes/IdeaDetail';
-import IdeaSubmitScene from './Scenes/IdeaSubmit';
+import LandingScene from 'Scenes/Landing';
+import PlaygroundScene from 'Scenes/Playground';
+import MapScene from 'Scenes/Map';
+import IdeaDetailScene from 'Scenes/IdeaDetail';
+import IdeaSubmitScene from 'Scenes/IdeaSubmit';
+
+import BottomNav from 'Components/BottomNav';
 
 function App() {
   return (
-    <StateProvider initialState={initialState} reducer={reducer}>
-      <Router>
-        <Route exact path={routes.root} component={LandingScene}/>
-        <Route path={routes.map} component={MapScene}/>
-        <Route path={routes.playground} component={PlaygroundScene}/>
-        <Route path={routes.ideaDetail} component={IdeaDetailScene}/>
-        <Route path={routes.ideaSubmit} component={IdeaSubmitScene}/>
-      </Router>
-    </StateProvider>
+    <ThemeProvider theme={theme}>
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <CssBaseline />
+        <Router>
+          <BottomNav
+            onChange={(event, newValue) => {
+              console.log('yyyySETVAL', newValue);
+            }}
+          />
+          <Route exact path={routes.root} component={LandingScene}/>
+          <Route path={routes.map} component={MapScene}/>
+          <Route path={routes.playground} component={PlaygroundScene}/>
+          <Route path={routes.ideaDetail} component={IdeaDetailScene}/>
+          <Route path={routes.ideaSubmit} component={IdeaSubmitScene}/>
+        </Router>
+      </StateProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/Components/BottomNav/index.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import routes from 'routes';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import MapIcon from '@material-ui/icons/Map';
+import LocationSearchingIcon from '@material-ui/icons/LocationSearching';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+
+
+const useStyles = makeStyles({
+  root: {
+    position: 'fixed',
+    bottom: 0,
+    width: '100%',
+  },
+});
+
+const BottomNav = ({ location, onChange }) => {
+  const classes = useStyles();
+
+  return (
+    <BottomNavigation
+      value={location.pathname}
+      onChange={onChange}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction
+        component={Link}
+        to={routes.map}
+        label="Map"
+        value={routes.map}
+        icon={<MapIcon />}
+      />
+      <BottomNavigationAction
+        component={Link}
+        to={routes.playground}
+        label="Nearby"
+        value={routes.playground}
+        icon={<LocationSearchingIcon />}
+      />
+      <BottomNavigationAction
+        component={Link}
+        to={routes.ideaSubmit}
+        label="Add Idea"
+        value={routes.ideaSubmit}
+        icon={<AddCircleIcon />}
+      />
+    </BottomNavigation>
+  );
+}
+
+
+export default withRouter(props => <BottomNav {...props}/>);
