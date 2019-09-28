ee4e316f5af7b374219b9d9b9cd799e62ef4435d
Raphael Brunner <raphael@raphaelbrunner.ch>
2019-09-28 14:48:24 +0200

    adding ServiceProvider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import routes from 'routes';
+import { StateProvider } from 'state';
 
 import LandingScene from './Scenes/Landing';
 import PlaygroundScene from './Scenes/Playground';
@@ -10,13 +11,15 @@ import IdeaSubmitScene from './Scenes/IdeaSubmit';
 
 function App() {
   return (
-    <Router>
-      <Route exact path={routes.root} component={LandingScene}/>
-      <Route path={routes.map} component={MapScene}/>
-      <Route path={routes.playground} component={PlaygroundScene}/>
-      <Route path={routes.ideaDetail} component={IdeaDetailScene}/>
-      <Route path={routes.ideaSubmit} component={IdeaSubmitScene}/>
-    </Router>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <Router>
+        <Route exact path={routes.root} component={LandingScene}/>
+        <Route path={routes.map} component={MapScene}/>
+        <Route path={routes.playground} component={PlaygroundScene}/>
+        <Route path={routes.ideaDetail} component={IdeaDetailScene}/>
+        <Route path={routes.ideaSubmit} component={IdeaSubmitScene}/>
+      </Router>
+    </StateProvider>
   );
 }
 
