76a6b4d476729d8a334234342b93c45632d9f4ea
Yves Buschor <yves.buschor@gmail.com>
2019-09-28 15:53:05 +0200

    add ping pong button 🏓

--- frontend/src/Scenes/Playground/index.jsx
+++ frontend/src/Scenes/Playground/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
-const PlaygroundView = (props) => (
-  <>
-    <h1>🏓</h1>
-  </>
-);
+import { useStateValue } from 'state';
+import { fetchIdeas } from 'doStuff';
+
+
+const PlaygroundView = (props) => {
+  const [{}, dispatch] = useStateValue();
+
+  return (
+    <>
+      <h1>🏓</h1>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => fetchIdeas(dispatch)}
+      >
+        Ping Pong
+      </Button>
+    </>
+  );
+}
 
 export default PlaygroundView;
